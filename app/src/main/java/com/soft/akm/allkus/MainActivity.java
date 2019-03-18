6d6fce074bed4fe2070926098774b924695d98c8
AMKCK <makowczenko93@gmail.com>
2019-03-18 03:53:47 +0100

    add new UI on ADD DATA

--- app/src/main/java/com/soft/akm/allkus/MainActivity.java
+++ app/src/main/java/com/soft/akm/allkus/MainActivity.java
@@ -1,13 +1,35 @@
 package com.soft.akm.allkus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button addButton,dataButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        addButton =(Button) findViewById(R.id.add_button);
+        dataButton =(Button) findViewById(R.id.data_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),add_data.class));
+            }
+        });
+
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),add_data.class));
+            }
+        });
     }
 }
